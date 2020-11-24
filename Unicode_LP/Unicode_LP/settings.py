8aa5dff3ad07496e7d4aabc68f59ed8a91d19f5d
Chris correia <chriscorreia2001@gmail.com>
2020-11-24 21:25:59 +0530

    Update settings.py

--- Unicode_LP/Unicode_LP/settings.py
+++ Unicode_LP/Unicode_LP/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'CHATBOX',
         'USER':'postgres',
-        'PASSWORD':'27082001',
+        'PASSWORD':'########',
         'HOST':'localhost'
     }
 }
@@ -121,7 +121,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'chriscorreia2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'gxyvwrbofllqazzj'
+EMAIL_HOST_PASSWORD = '##########'
 EMAIL_USE_TLS = True
 
 
